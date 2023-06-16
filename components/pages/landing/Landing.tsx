7459df3801ca6365c3dbbcef580b2c72d3171e01
Rainata Putra <kriegcom77@gmail.com>
2023-06-16 09:53:31 +0700

    Adding Team Page (Not Done)

--- components/pages/landing/Landing.tsx
+++ components/pages/landing/Landing.tsx
@@ -1,12 +1,14 @@
 import Container from "@/components/common/Container";
 import Home from "./Home";
 import Values from "./Values";
+import Team from "./Team";
 
 const Landing = () => {
   return (
     <Container className="!overflow-x-hidden !mt-0 !py-0 !px-0">
       <Home />
       <Values />
+      <Team />
     </Container>
   );
 };
---
+++ components/pages/landing/Team.tsx
@@ -0,0 +1,76 @@
+const Team = () => {
+    return ( 
+    <div className="block mx-auto md:flex md:overflow-x-auto">
+        <div className="md:flex-shrink-0 w-64 bg-white text-black rounded-md px-2 py-10 m-5 drop-shadow-lg">
+            <div className="mx-auto w-24 h-24 rounded-full bg-white text-black overflow-hidden">
+                <img className="w-full h-full object-cover" src="https://drive.google.com/u/0/uc?id=1Uw6Kcj6IFynv3JRpwjBLRFbKU6EfyNpg&export=download" alt="" />
+                
+            </div>
+            <div className="text-black mx-auto w-3/4 py-5">
+                <div className="text-black text-center text-2xl">Rainata Putra</div>
+                <div className="text-black text-center">M038DSX1743</div>
+                <div className="whitespace-normal text-black text-center mt-5 font-serif">"Makmur is a different inovation. I'm excited to see the app making a positive impact in reducing food waste."</div>
+            </div>
+        </div>
+
+        <div className="flex-shrink-0 w-64 bg-white text-black rounded-md px-2 py-10 m-5 drop-shadow-lg">
+            <div className="mx-auto w-24 h-24 rounded-full bg-white text-black overflow-hidden">
+                <img className="w-full h-full object-cover" src="https://drive.google.com/u/0/uc?id=16YrExv16k0KtHJnwtoDEgffX31n0YdD-&export=download" alt="" />
+            </div>
+            <div className="text-black mx-auto w-3/4 py-5">
+                <div className="text-black text-center text-2xl">Muhammad Amin</div>
+                <div className="text-black text-center">M038DSX2549</div>
+                <div className="whitespace-normal text-black text-center mt-5 font-serif">"It's great to know that our work directly addresses the issue of food waste and promotes sustainability. "</div>
+            </div>
+        </div>
+
+        <div className="flex-shrink-0 w-64 bg-white text-black rounded-md px-2 py-10 m-5 drop-shadow-lg">
+            <div className="mx-auto w-24 h-24 rounded-full bg-white text-black overflow-hidden">
+                <img className="w-full h-full object-cover" src="https://drive.google.com/u/0/uc?id=1X43SpQpfz4Z56QveFTwcefFJFxQB6JYF&export=download" alt="" />
+            </div>
+            <div className="text-black mx-auto w-3/4 py-5">
+                <div className="text-black text-center text-2xl">Indra Mahaarta</div>
+                <div className="text-black text-center">C181DSX1949</div>
+                <div className="whitespace-normal text-black text-center mt-5 font-serif">"The Makmur app is a project I'm truly proud to be a part of. I hope Makmur becomes a powerful tool in combating food waste globally."</div>
+            </div>
+        </div>
+
+        <div className="flex-shrink-0 w-64 bg-white text-black rounded-md px-2 py-10 m-5 drop-shadow-lg">
+            <div className="mx-auto w-24 h-24 rounded-full bg-white text-black overflow-hidden">
+                <img className="w-full h-full object-cover" src="https://drive.google.com/u/0/uc?id=1kGxQz4A5WBt1EZ2-R9Izg4oRJWJL-qUe&export=download" alt="" />
+            </div>
+            <div className="text-black mx-auto w-3/4 py-5">
+                <div className="text-black text-center text-2xl">Lintang Syuhada</div>
+                <div className="text-black text-center">C169DSX1349</div>
+                <div className="whitespace-normal text-black text-center mt-5 font-serif">"It's amazing to think that our app can make a difference in reducing food waste by making it easier for people to find and purchase leftover food."</div>
+            </div>
+        </div>
+
+        <div className="flex-shrink-0 w-64 bg-white text-black rounded-md px-2 py-10 m-5 drop-shadow-lg">
+            <div className="mx-auto w-24 h-24 rounded-full bg-white text-black overflow-hidden">
+                <img className="w-full h-full object-cover" src="https://img.freepik.com/premium-photo/anime-girl-watching-sunset-3d-illustration_717906-1415.jpg?w=2000" alt="" />
+            </div>
+            <div className="text-black mx-auto w-3/4 py-5">
+                <div className="text-black text-center text-2xl">Adlan Walid</div>
+                <div className="text-black text-center">A181DSX3033</div>
+                <div className="whitespace-normal text-black text-center mt-5 font-serif">"A well-designed app raises awareness about reducing food waste and that is Makmur"</div>
+            </div>
+        </div>
+
+        <div className="flex-shrink-0 w-64 bg-white text-black rounded-md px-2 py-10 m-5 drop-shadow-lg">
+            <div className="mx-auto w-24 h-24 rounded-full bg-white text-black overflow-hidden">
+                <img className="w-full h-full object-cover" src="https://img.freepik.com/premium-photo/anime-girl-watching-sunset-3d-illustration_717906-1415.jpg?w=2000" alt="" />
+            </div>
+            <div className="text-black mx-auto w-3/4 py-5">
+                <div className="text-black text-center text-2xl">Jason Widodo</div>
+                <div className="text-black text-center">A181DSX3420</div>
+                <div className="whitespace-normal text-black text-center mt-5 font-serif">"Our app has the potential to create a significant change by connecting people with leftover food that would have otherwise gone to waste."</div>
+            </div>
+        </div>
+
+    
+    </div> 
+);
+}
+ 
+export default Team;
